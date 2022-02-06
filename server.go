44b22e2c0b66ba5160248770b7de34a95ae34a1d
Chavez Harris <codedbychavez@gmail.com>
2022-02-06 16:16:46 -0400

    Added fiber logger

--- server.go
+++ server.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/codedbychavez/go-rabbitmq-api/controllers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+
 )
 
 
 func main( ) {
 	app := fiber.New()
+
+
+	// Setup logger --> Logs requests to the console
+	app.Use(logger.New(logger.Config{
+		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path}\n",
+	}))
+
+
 	taskController := controllers.NewTaskController()
 
 	app.Post("/api/v1/sendtask", taskController.ReceiveTask)
